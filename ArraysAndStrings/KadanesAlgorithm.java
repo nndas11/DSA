ac4954761845193cb9193c671dfbcb58d50719de
nihad <nihad.m@captainfresh.in>
2025-04-04 22:01:06 +0530

    added KadanesAlgorithm

---
+++ ArraysAndStrings/KadanesAlgorithm.java
@@ -0,0 +1,44 @@
+package ArraysAndStrings;
+
+
+// Time Complexity: O(n)
+// Used to find the largest sub-array sum.
+
+public class KadanesAlgorithm {
+    public static void main(String[] args) {
+        int[] nums = {4,-5,2,-2,7,5};
+        int maxSum = Kadanes(nums);
+        System.out.println("Maximum Sub-array sum is:" + maxSum);
+    }
+
+     static int KadanesAlgo(int[] nums){
+        int n = nums.length;
+        int maxSum = nums[0];
+        int currSum = 0;
+
+        for(int i=0;i<n;i++){
+            currSum += nums[i];
+            maxSum = Math.max(maxSum,currSum);
+            if(currSum < 0)
+                currSum = 0;
+        }
+        return maxSum;
+    }
+
+    static int Kadanes(int[] nums){
+        int n = nums.length;
+        int maxSum = nums[0];
+        int currSum = 0;
+
+        for(int num: nums){
+            currSum += num;
+            maxSum = Math.max(maxSum,currSum);
+            currSum = Math.max(currSum,0);
+        }
+        return maxSum;
+    }
+}
+
+
+
+
