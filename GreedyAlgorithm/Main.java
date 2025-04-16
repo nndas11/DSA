6f1302c03c49be531b7728d0244d17b2e5112a36
nihad <nihad.m@captainfresh.in>
2025-04-16 15:42:26 +0530

    added greedy notes

---
+++ GreedyAlgorithm/Main.java
@@ -0,0 +1,21 @@
+package GreedyAlgorithm;
+
+//  A greedy algorithm is a strategy that makes the best possible choice at each step,
+//  hoping that these local optimum choices lead to a global optimum solution.
+
+//  Definition (In Simple Terms)
+//  At every step, a greedy algorithm:
+//  Picks the option that looks the best right now (locally optimal).
+//  Doesn’t reconsider previous choices.
+//  Aims to reach the optimal solution in the end — but doesn’t always guarantee it, unless the problem satisfies certain conditions.
+
+//  Greedy Doesn't Always Work
+//  Some problems look greedy, but need DP instead.
+
+//  How to Know if Greedy Works?
+//  Does making a greedy choice now guarantee the best final answer?
+//  Try with a few counterexamples or prove with induction.
+
+
+public class Main {
+}
