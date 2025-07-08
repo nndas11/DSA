401f27c847b2fef801813b472e9b868febd61d1b
nndas11 <mohammednihadkp.mec@gmail.com>
2025-07-08 15:11:29 +0530

    added minimum spanning tree

---
+++ Graphs/MinimumSpanningTress/Main.java
@@ -0,0 +1,25 @@
+package Graphs.MinimumSpanningTress;
+
+//  Given Undirected weighted graph.
+//  N nodes
+//  M edges
+
+//  Minimum Spanning Tree(MST) is graph with N nodes and N-1 edges and all nodes are reachable from each other
+//  We can have many spanning trees for a graph, it depends on the graph.
+//  And the tree with minimum sum(sum of weights) is the Minimum Spanning Tree.
+
+//*IMPORTANT* POINTS FOR MST:
+//        1-> If each edge has a distinct weight then there will be only one & unique MST.
+//        2-> A complete undirected graph can have n^(n-2) number of Spanning Trees. For example : Consider a triangle which have 3 vertices and 3 edges so n=3 || 3^(3-2) => 3. Hence , it would have 3 spanning trees.
+//        3-> From a Complete graph by removing max(e-n+1) edges, we can construct a Spanning Tree.
+
+
+//  Algorithms to find MST:
+//      1. Prim's Algorithm
+//      2. Kruskal's Algorithm -> Disjoin Set is a prerequisite.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
