7d4224f61d5e31bd066917f53bc8d01293714228
nndas11 <mohammednihadkp.mec@gmail.com>
2025-07-12 22:07:29 +0530

    added tarjans algorithm

---
+++ Graphs/TarjansAlgorithm/Main.java
@@ -0,0 +1,21 @@
+package Graphs.TarjansAlgorithm;
+
+//  Tarjan's Algorithm
+//  Refer Leetcode question -> Critical Connections in a Network --> Asked in AMAZON interview.
+
+//  Bridges in a graph -> Bridge is an edge when removed, makes it broken down into two or more components.
+//  To find the bridge -> Using Tarjan's Algorithm -> some changes to DFS algorithm.
+
+//  DFS + time of insertion --> Concept used in many questions.
+
+//  2 Arrays Required:
+//      1.  time[] ->   DFS time insertion -> the time at which the node was reached while doing DFS.
+//      2.  low[]  ->   minimum lowest time insertion of all adjacent nodes "apart from parent".
+
+//  Even if visited -> we can update the low
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
