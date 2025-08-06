969f78d52f026d64e77f627efae0b05b7360b369
nndas11 <mohammednihadkp.mec@gmail.com>
2025-08-06 16:22:45 +0530

    added binary search notes.

---
+++ BinarySearch/Main.java
@@ -0,0 +1,16 @@
+package BinarySearch;
+
+//  While doing Binary Search Problem -> all depends on sorting
+//  Also the idea about Binary Search is that, we should be able to reduce our search space by half each time.
+//  The array should be sorted fully or some portion should be sorted -> Queue to use Binary Search.
+//  Like rotated or may be trying to find the breaking point.
+//  Think of LOWER-BOUND and OUTER-BOUND -> many question can be solved using this.
+//  Then we can compare the mid and mid - 1 or mid and mid + 1 element.
+//  In some question we compare the mid with left and right and so on.
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
